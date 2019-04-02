f2fda1cc5440317731f4e975af1ebb9895060506
Kimmo Haapakoski <t7haki00@students.oamk.fi>
2019-04-02 12:58:23 +0300

    Changed array to string

--- Mobiiliprojekti/watch/src/main/java/com/example/mobiiliprojekti/MessageService.java
+++ Mobiiliprojekti/watch/src/main/java/com/example/mobiiliprojekti/MessageService.java
@@ -15,11 +15,10 @@ public class MessageService extends WearableListenerService {
         if (messageEvent.getPath().equals("/my_path")) {
             try{
                 final String message = new String(messageEvent.getData());
-                String[] array = message.split(";");
                 Log.d("kimmo", "trying to launch");
                 PackageManager pm = this.getPackageManager();
                 Intent intent = pm.getLaunchIntentForPackage(this.getPackageName());
-                intent.putExtra("message", array);
+                intent.putExtra("message", message);
                 this.startActivity(intent);
             }catch(Exception e){
                 Log.d("kimmo", e.toString());
