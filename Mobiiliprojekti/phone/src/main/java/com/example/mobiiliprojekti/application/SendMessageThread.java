3ab9f0556636b6ab13fbd2d20a06e5707400b122
Kimmo Haapakoski <t7haki00@students.oamk.fi>
2019-04-16 13:43:10 +0300

    New class for sending message. Now messages can be sent from all activities and classes.

---
+++ Mobiiliprojekti/phone/src/main/java/com/example/mobiiliprojekti/application/SendMessageThread.java
@@ -0,0 +1,58 @@
+package com.example.mobiiliprojekti.application;
+
+import android.content.Context;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import java.util.List;
+
+public class SendMessageThread extends Thread {    //Inner class for sending messages to clock
+    private String path;                           //An unique identifier for wearable to access the message
+    private String message;                        //The message to wearable
+    private Context context;
+    protected Handler myHandler;
+
+    public SendMessageThread(String mPath, String mMessage, Context context) {
+        path = mPath;
+        message = mMessage;
+        this.context = context;
+    }
+
+    public void sendmessage(String messageText) {   //Function that is called inside the run-function in NewThread class
+        Bundle bundle = new Bundle();
+        bundle.putString("messageText", messageText);
+        Message msg = myHandler.obtainMessage(); //Returns a new message from  the global message pool
+        msg.setData(bundle);
+        myHandler.sendMessage(msg); //Pushes a message onto the end of the message queue
+    }
+
+    public void run() {
+        //Gets a list of connected nodes to which the device is connected
+        Task<List<Node>> wearableList =
+                Wearable.getNodeClient(context).getConnectedNodes();
+        try {
+            List<Node> nodes = Tasks.await(wearableList);
+            for (Node node : nodes) {
+                Task<Integer> sendMessageTask =
+                        Wearable.getMessageClient(context).sendMessage(node.getId(), path, message.getBytes());
+                try {
+                    Integer result = Tasks.await(sendMessageTask);
+
+                    sendmessage(message);
+
+                } catch (Exception exception) {
+
+                }
+            }
+        } catch (Exception exception) {
+
+        }
+    }
+}
